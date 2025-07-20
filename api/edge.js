1eaa69dd655328d0411bc5942b243b8dde736c9f
Alejandrocsdev <alejandrocsdev@gmail.com>
2025-07-20 13:02:14 +0800

    update vercel edge, use export default instead of module.exports

--- api/edge.js
+++ api/edge.js
@@ -1,5 +1,5 @@
 // This is a Vercel Serverless Function that returns proxy-related info
-module.exports = (req, res) => {
+export default function logClientInfo(req, res) {
   const forwarded = req.headers['x-forwarded-for']
   const ua = req.headers['user-agent']
   res.status(200).json({ forwarded, ua })
